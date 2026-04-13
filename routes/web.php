90cea7833216b66bfa54b02e306b94fd40a606e5
Udhayan26 <32291128+Udhayan26@users.noreply.github.com>
2026-04-13 17:14:51 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InscriptionController;
 
-Route::get('/', [InscriptionController::class, 'index']);
-Route::get('/dashboard', [InscriptionController::class, 'dashboard']);
-Route::get('/locations', [InscriptionController::class, 'getLocations']);
+// The Main Shell
+Route::get('/', [InscriptionController::class, 'index'])->name('home');
+
+// Modular AJAX Routes (The 6 Divs)
+Route::prefix('api')->group(function () {
+    Route::get('/dashboard', [InscriptionController::class, 'getDashboard']);
+    Route::get('/locations', [InscriptionController::class, 'getLocations']);
+    Route::get('/search', [InscriptionController::class, 'getSearch']);
+    Route::get('/books', [InscriptionController::class, 'getBooks']);
+    Route::get('/publications', [InscriptionController::class, 'getPublications']);
+    Route::get('/map', [InscriptionController::class, 'getMap']);
+});

ec02bd22bd43c1f9f7ac3ab5890eaa846bf03d73
Udhayan26 <32291128+Udhayan26@users.noreply.github.com>
2026-04-13 20:36:03 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,9 @@
 <?php
 use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
-
-// Standard Laravel entry point
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
-
 require __DIR__.'/../vendor/autoload.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());

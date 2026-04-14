c18ab6b835239895bf4d1eb04f01e401ce085ec4
Udhayan26 <32291128+Udhayan26@users.noreply.github.com>
2026-04-14 11:19:14 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,17 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 use Illuminate\Http\Request;
+
 define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
+
+// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
+
+// Run the application...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
